2c787b45c2e56b1df05d26acb1bfd356db414a6c
longtd <longistore@mail.com>
2025-04-25 09:41:36 +0700

    no message

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import {ToastProvider} from "./components/ToastProvider.jsx";
 import {AuthProvider} from "./contexts/AuthContext.jsx";
+import { Buffer } from 'buffer'
+window.Buffer = Buffer
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
